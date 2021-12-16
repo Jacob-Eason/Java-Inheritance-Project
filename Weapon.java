065b73e145293a9b534d8c4d6bd4eb2cee42dae3
Ustr1221 <khamtsang@gmail.com>
2021-12-16 14:15:48 -0800

    added Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Weapon{
+    private ArrayList<String> attachments; 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getAttatchments() {
+        return attachments;
+    }
+   
+}
